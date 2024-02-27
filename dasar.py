41172c06b41e2346c5f102dcab1690704b989649
Your Name <you@example.com>
2024-02-27 06:21:04 -0800

    Commit Pertamaku 2024

---
+++ dasar.py
@@ -0,0 +1,7 @@
+print ("Halo sobat Inners!")
+nama = "Fajar Ramadhan"
+umur = "21"
+pekerjaan = "manajer di FIFA Mobile"
+
+print("Halo perkenalkan nama saya" + nama + ", umur saya" + umur +", pekerjaan saya" + pekerjaan + "")
+print("Terima Kasih !")
